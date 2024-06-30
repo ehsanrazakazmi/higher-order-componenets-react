94431b33d2dcaf06b946a2ee4fe49fb14d387438
ehsanrazakazmi <ehsankazmi7@gmail.com>
2024-06-30 13:10:45 +0500

    pushed
    pushed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchTodos from './TodoList';
+import SearchUsers from './UsersList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Higher Order Componenet</h2>
+      <div className='section'>
+        <div>
+        <SearchUsers></SearchUsers>
+
+        </div>
+        <div>
+          <SearchTodos></SearchTodos>
+        </div>
+
+      </div>
     </div>
   );
 }
